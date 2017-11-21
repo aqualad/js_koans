ecb2aed9962b347903b62fa9ad6198635d44d74f
Chris Hafley <chafley@onshift.com>
2017-11-20 23:24:39 -0500

    adds es6 and helpers for tests

---
+++ test/es6/destructuring.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-unused-vars */
+describe('Destructuring', () => {
+    describe('Arrays', () => {
+        let array = ['a', 'b', 'c'];
+        It('allows declaration assignment', () => {
+            let [a,b,c, d] = array;
+            is(___, a);
+            is(___, b);
+            is(___, c);
+            is(___, d);
+        });
+
+        It('allows parameter assignment', () => {
+            const firstEqual = ([a], [b]) => a === b;
+            is(___, firstEqual([0, 1], ['0', '1']));
+        });
+
+        It('allows rest assignment', () => {
+            let [a, ...bs] = array;
+            isEqual(___, bs);
+        });
+
+        It('breaks on undefined or null', () => {
+            const unsafe = ([a,b,c]) =>  [a,b,c];
+            expect(unsafe).to.throw(TypeError);
+
+            const safe = ([a,b,c] = []) => [a,b,c];
+            expect(safe).to.not.throw(TypeError);
+        });
+
+        It('is just syntax sugar', () => {
+            let x = array[0],
+                ys = array.slice(1);
+            let [a,...bs] = array;
+            is(___, x);
+            is(___, ys);
+        });
+
+        It('allows for defaults as well', () => {
+            let [a, b, c, d, e = 'E'] = array;
+            is(___, d);
+            is(___, e);
+        });
+
+        It('allows re-assignment and swapping', () => {
+            let [a,b] = array;
+            [b,a] = [a,b];
+            is(___, a);
+            is(___, b);
+        });
+
+    });
+    describe('Objects', () => {
+        It('allows declaration assignment', () => {
+            const {x, y, z} = {
+                x: 1,
+                y: 2
+            };
+            is(___, x);
+            is(___, y);
+            is(___, z);
+        });
+        It('breaks on undefined or null', () => {
+            let x,y;
+            const unsafe = ({x, y}) => x + y;
+            expect(unsafe).to.throw(___);
+            const safeDefaults = ({x,y} = {}) => x + y;
+            expect(safeDefaults).to.not.throw(___);
+        });
+    });
+});
--- test/oop.js
+++ test/oop.js
@@ -13,8 +13,9 @@ describe('Prototypes:', () => {
             is(___, myObject.hasOwnProperty instanceof Function);
         });
 
-        It(`inherits properties from the global Objects Prototype`, () => {
-            is(___, myObject.hasOwnProperty === Object.prototype.hasOwnProperty);
+        It(`inherits properties from its constructor's prototype`, () => {
+            is(___, myObject.constructor);
+            is(___, myObject.hasOwnProperty === myObject.constructor.prototype.hasOwnProperty);
         });
 
         It(`does not own inherited properties`, () => {
@@ -22,10 +23,100 @@ describe('Prototypes:', () => {
         });
 
         It(`can shadow prototype properties`, () => {
-            myObject.hasOwnProperty = identity;
+            myObject.hasOwnProperty = Identity;
             is(15, myObject.hasOwnProperty(15));
             is(___, myObject.hasOwnProperty === Object.prototype.hasOwnProperty);
-            is(___, Object.prototype.hasOwnProperty.call(myObject, 'hasOwnProperty'));
+        });
+
+        It('can still use the prototype property after shadowing',() => {
+            myObject.hasOwnProperty = Identity;
+            is(___, myObject.constructor);
+            is(___, myObject.constructor.prototype.hasOwnProperty.call(myObject, 'hasOwnProperty'));
+        });
+    });
+
+    describe('For Literals', () => {
+        It('exists on array, object, function literals', () => {
+            is(___, {}          .constructor.prototype);
+            is(___, []          .constructor.prototype);
+            is(___, function(){}.constructor.prototype);
+            is(___, (() => {})  .constructor.prototype);
+        });
+
+        It('is possible to create an object with zero properties and no prototype', () => {
+            let trueHashMap = Object.create(null);
+            is(___, trueHashMap instanceof Object);
+            is(___, trueHashMap.hasOwnProperty);
+        });
+    });
+
+    describe('Are just object references', () => {
+        let A, B;
+
+        let reset = () => (
+            A = { a: 1 },
+            B = { b: 2 }
+        );
+        beforeEach(reset);
+
+        It('is possible to create a new object that inherits arbitrarily from another', () => {
+            let C = Object.create(A);
+            is(___, C.a);
+            C.a = 3;
+            is(___, A.a);
+            is(___, Object.getPrototypeOf(C));
+            is(___, C.constructor);
+            is(___, C instanceof A);
+        });
+
+        It('is possible to emulate (static) multiple inheritance (mixins)', () => {
+            let C = Object.create(Object.assign({}, A, B));
+            is(___, Object.getPrototypeOf(C));
+            is(___, C.a);
+            is(___, C.b);
+        });
+
+        It('is possible to dynamically update the prototype', () => {
+            let C = Object.create(A);
+            is(___, C.c);
+            A.c = 3;
+            is(___, C.c);
+        });
+
+        It('is possible to dynamically and destructively update the prototype', () => {
+            let C = Object.create(A);
+            let D = Object.create(A);
+            C.constructor.prototype.a = 3;
+            is(___, A.a);
+            is(___, D.a);
+        });
+    });
+
+    describe('Acts as linked list', () => {
+        It('traverses the chain of prototypes to find the value of a prop', () => {
+            let A = {a:1},
+                B = {b:2},
+                C = {c:3};
+            Object.setPrototypeOf(B, A);
+            Object.setPrototypeOf(C, B);
+            is(___, C.a);
+            is(___, C.b);
+            is(___, C.c);
+        });
+    });
+
+    describe('Gotchas', () => {
+        it('will iterate inherited properties', () => {
+            let keys = [];
+            let A = {a:1};
+            let B = {b:2};
+            Object.setPrototypeOf(B, A);
+            let C = Object.create(B);
+            C.c = 3;
+            for(let key in C) {
+                keys.push(key);
+            }
+            isEqual(keys, Object.keys(C));
         });
     });
 });
